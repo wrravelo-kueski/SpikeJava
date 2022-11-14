f0e25b952689ceda19e91c59af31bf6e1bead24a
William Ravelo Mendez <william.ravelo@kueski.com>
2022-11-14 14:57:08 -0500

    CRUD mocking data

--- src/main/java/com/pipelines/spike/controllers/BlogController.java
+++ src/main/java/com/pipelines/spike/controllers/BlogController.java
@@ -1,12 +1,59 @@
 package com.pipelines.spike.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import java.util.List;
+import java.util.Map;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.pipelines.spike.mocks.BlogMockedData;
+import com.pipelines.spike.models.Blog;
 
 @RestController
 public class BlogController {
-  @RequestMapping("/")
-  public String index() {
-    return "Hello world";
+
+  BlogMockedData blogMockedData = BlogMockedData.getInstance();
+  
+  @GetMapping("/blog")
+  public List<Blog> index() {
+    return blogMockedData.fetchBlogs();
+  }
+
+  @GetMapping("/blog/{id}")
+  public Blog show(@PathVariable String id) {
+    int blogId = Integer.parseInt(id);
+    return blogMockedData.getBlogById(blogId);
+  }
+
+  @PostMapping("/blog/search")
+  public List<Blog> search(@RequestBody Map<String, String> body) {
+    String searchTerm = body.get("text");
+    return blogMockedData.searchBlogs(searchTerm);
+  }
+
+  @PostMapping("/blog")
+  public Blog create(@RequestBody Map<String, String> body) {
+    int id = Integer.parseInt(body.get("id"));
+    String title = body.get("title");
+    String content = body.get("content");
+    return blogMockedData.createBlog(id, title, content);
+  }
+
+  @PutMapping("/blog/{id}")
+  public Blog update(@PathVariable String id, @RequestBody Map<String, String> body) {
+    int blogId = Integer.parseInt(id);
+    String title = body.get("title");
+    String content = body.get("content");
+    return blogMockedData.updateBlog(blogId, title, content);
+  }
+
+  @DeleteMapping("blog/{id}")
+  public boolean delete(@PathVariable String id) {
+    int blogId = Integer.parseInt(id);
+    return blogMockedData.delete(blogId);
   }
 }
