02d7357725bc10a2f4ba053ee48061a0c3745676
William Ravelo Mendez <william.ravelo@kueski.com>
2022-11-14 14:31:14 -0500

    initial

---
+++ src/test/java/com/pipelines/spike/SpikeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pipelines.spike;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpikeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
